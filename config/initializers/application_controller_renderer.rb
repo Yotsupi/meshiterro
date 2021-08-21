059d390e0849ebbd817c9f62003134021e3d3f9c
Yotsupi <kayoshida.1830@gmail.com>
2021-08-21 12:46:19 +0000

    [Add]認証機能

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'a67b709d9e41a5354f1917298d0d15da4d1c6e9f5d8b5b7ddc51d6972ac202963519f11c0cb9e29c847bb5a0849304cdd89c3dab284fe7bf276c4016bbffc018'
