11cfcb6c82f4298d584122702251e0542639428c
Yotsupi <kayoshida.1830@gmail.com>
2021-08-22 15:01:40 +0000

    [Add]いいね機能

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,5 +1,5 @@
 class PostCommentsController < ApplicationController
-  
+
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
@@ -7,14 +7,14 @@ class PostCommentsController < ApplicationController
     comment.save
     redirect_to post_image_path(post_image)
   end
-  
+
   def destroy
     PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
     redirect_to post_image_path(params[:post_image_id])
   end
-  
+
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
-  
+
 end
